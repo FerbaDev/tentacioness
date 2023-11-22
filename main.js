6771100fa2142494959ead5317cff3ee3270510f
FerbaDev <elferbarron1@gmail.com>
2023-11-22 13:43:52 -0300

    se cargan nuevos htmls

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
-let productos = [];
+// let productos = [];
 
-fetch("../productos.json")
-  .then((response) => response.json())
-  .then((data) => {
-    productos = data;
-    cargarProductos(productos);
-  });
+// fetch("../productos.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     productos = data;
+//     cargarProductos(productos);
+//   });
 
-const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
-const tituloPrincipal = document.querySelector("#titulo-principal");
+// const contenedorProductos = document.querySelector("#contenedor-productos");
+// const botonesCategorias = document.querySelectorAll(".boton-categoria");
+// const tituloPrincipal = document.querySelector("#titulo-principal");
 
-function cargarProductos(productosElegidos) {
-  contenedorProductos.innerHTML = "";
+// function cargarProductos(productosElegidos) {
+//   contenedorProductos.innerHTML = "";
 
-  productosElegidos.forEach((producto) => {
-    const div = document.createElement("div");
-    div.classList.add("producto");
-    div.innerHTML = `
-            
-            <div class="producto-detalles">
-                <img src=${producto.image} alt="" class="producto-imagen">
-                <div class="producto-info">
-                  <h3 class="producto-titulo">${producto.titulo}</h3>
-                  <p class="producto-precio">$${producto.precio}</p>
-                </div>
-                
-            </div>
-        `;
+//   productosElegidos.forEach((producto) => {
+//     const div = document.createElement("div");
+//     div.classList.add("producto");
+//     div.innerHTML = `
 
-    contenedorProductos.append(div);
-  });
-}
+//             <div class="producto-detalles">
+//                 <img src=${producto.image} alt="" class="producto-imagen">
+//                 <div class="producto-info">
+//                   <h3 class="producto-titulo">${producto.titulo}</h3>
+//                   <p class="producto-precio">$${producto.precio}</p>
+//                 </div>
 
-botonesCategorias.forEach((boton) => {
-  boton.addEventListener("click", (e) => {
-    botonesCategorias.forEach((boton) => boton.classList.remove("active"));
-    e.currentTarget.classList.add("active");
+//             </div>
+//         `;
 
-    if (e.currentTarget.id != "todos") {
-      const productoCategoria = productos.find(
-        (producto) => producto.categoria.id === e.currentTarget.id
-      );
-      tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-      const productosBoton = productos.filter(
-        (producto) => producto.categoria.id === e.currentTarget.id
-      );
-      cargarProductos(productosBoton);
-    } else {
-      tituloPrincipal.innerText = "Todos los productos";
-      cargarProductos(productos);
-    }
-  });
-});
+//     contenedorProductos.append(div);
+//   });
+// }
+
+// botonesCategorias.forEach((boton) => {
+//   boton.addEventListener("click", (e) => {
+//     botonesCategorias.forEach((boton) => boton.classList.remove("active"));
+//     e.currentTarget.classList.add("active");
+
+//     if (e.currentTarget.id != "todos") {
+//       const productoCategoria = productos.find(
+//         (producto) => producto.categoria.id === e.currentTarget.id
+//       );
+//       tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+//       const productosBoton = productos.filter(
+//         (producto) => producto.categoria.id === e.currentTarget.id
+//       );
+//       cargarProductos(productosBoton);
+//     } else {
+//       tituloPrincipal.innerText = "Todos los productos";
+//       cargarProductos(productos);
+//     }
+//   });
+// });
