5e6333ec498dfd6df28af9a449ddb4863667a0b6
FerbaDev <elferbarron1@gmail.com>
2024-01-26 07:54:56 -0300

    se agregan clickeables y se agrega seccion ofertas

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const mujeres = document.getElementById("mujeres");
 const hombres = document.getElementById("hombres");
 const ninios = document.getElementById("ninios");
 const fragancias = document.getElementById("fragancias");
+const ofertas = document.getElementById("ofertas");
 
 mujeres.addEventListener("click", () => {
   //enlace de la pagina que quieras
@@ -21,4 +22,9 @@ ninios.addEventListener("click", () => {
 fragancias.addEventListener("click", () => {
   //enlace de la pagina que quieras
   window.location.href = "./fragancias.html";
+});
+
+ofertas.addEventListener("click", () => {
+  //enlace de la pagina que quieras
+  window.location.href = "./ofertas.html";
 });
